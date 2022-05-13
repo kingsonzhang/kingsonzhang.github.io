52be5843567b0d10171a3b9c4707216480dbd6e2
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-05-12 19:21:14 -0700

    May 12, 2022 Portfolio Update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+setInterval(changeColor, 1000);
+
+function changeColor(){
+    const RED = Math.floor(Math.random() * 255);
+    const GREEN = Math.floor(Math.random() * 255);
+    const BLUE = Math.floor(Math.random() * 255);
+    document.querySelector("body").style.backgroundColor = `rgb(${RED}, ${GREEN}, ${BLUE})`;
+
+    if ((RED + GREEN + BLUE) / 3 < (255 / 2)){
+        document.querySelector("h1").style.color = "white";
+        document.querySelector("p").style.color = "white";
+    }
+    else{
+        document.querySelector("h1").style.color = "black";
+        document.querySelector("p").style.color = "black";
+    }
+}
