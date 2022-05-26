d7d4ce053307ec6ea9eccfce457ef48a5903d56e
Kingson Zhang <kingson.x.zhang@gmail.com>
2022-05-26 13:32:21 -0700

    Added icons back into artifact list

--- GenshinArtifactSorter/js/GenshinArtifactScript.js
+++ GenshinArtifactSorter/js/GenshinArtifactScript.js
@@ -1,35 +1,51 @@
+//Keep a record of the current artifact set selected
 let current = "";
+const HOVERBACKGROUNDCOLOR = "rgba(50, 50, 50, .5)";
+const NEUTRALBACKGROUNDCOLOR = "rgba(50, 50, 50, 0)";
+const SELECTEDBACKGROUNDCOLOR = "rgba(50, 50, 50, 1)";
 
+//Add an EventListener for hover on artifact set name
+//Highlight background to let the user know which artifact set is currently hovered
 document.querySelectorAll(".ArtifactSet").forEach(x => x.addEventListener("mouseover", function onHover(event){
-    event.target.style.backgroundColor = "rgba(50, 50, 50, .5)";
+    event.target.style.backgroundColor = HOVERBACKGROUNDCOLOR;
 }));
 
+//Add an EventListner for mouse exit on artifact set name
+//Un-highlight background to the let the user know which artifact set was just left
+//Depending on if the artifact set hovered was selected or not
+//Change the state to previous
 document.querySelectorAll(".ArtifactSet").forEach(x => x.addEventListener("mouseout", function exitHover(event){
     if (event.target.id != current)
-        event.target.style.backgroundColor = "rgba(50, 50, 50, 0)";
+        event.target.style.backgroundColor = NEUTRALBACKGROUNDCOLOR;
     else
-    event.target.style.backgroundColor = "rgba(50, 50, 50, 1)";
+    event.target.style.backgroundColor = SELECTEDBACKGROUNDCOLOR;
 }));
 
+//On click, select the artifact set
+//Changing background color to selected
+//Changing artifact set text to white color
+//Revert previous artifact set collected to neutral state
+//Display the pieces of the artifact set selected
 document.querySelectorAll(".ArtifactSet").forEach(x => x.addEventListener("click", function onClick(event){
     if (current != ""){
         let previousSelected = document.getElementById(current);
-        previousSelected.style.backgroundColor = "rgba(50, 50, 50, 0)";
+        previousSelected.style.backgroundColor = NEUTRALBACKGROUNDCOLOR;
         previousSelected.style.color = "black";
     }
     current = event.target.id;
 
     let currentlySelected = document.getElementById(current);
-    currentlySelected.style.backgroundColor = "rgba(50, 50, 50, 1)";
+    currentlySelected.style.backgroundColor = SELECTEDBACKGROUNDCOLOR;
     currentlySelected.style.color = "white";
 
     changeArtifacts(current);
 }));
 
+//Loop through each type of artifact pieces and get their img
+//Update the cooresponding img srcs
 function changeArtifacts(eventId){
-    document.getElementById("Flower").src=`./images/${eventId}/${eventId}Flower.jpg`
-    document.getElementById("Feather").src=`./images/${eventId}/${eventId}Feather.jpg`
-    document.getElementById("Timepiece").src=`./images/${eventId}/${eventId}Timepiece.jpg`
-    document.getElementById("Goblet").src=`./images/${eventId}/${eventId}Goblet.jpg`
-    document.getElementById("Circlet").src=`./images/${eventId}/${eventId}Circlet.jpg`
+    const PIECES = ["Flower", "Feather", "Timepiece", "Goblet", "Circlet"];
+    PIECES.forEach(x =>{
+        document.getElementById(x).src = `./images/${eventId}/${eventId}${x}.jpg`;
+    });
 }
